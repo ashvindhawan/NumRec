39e752835a8e07f564d824eea3a903cae498c572
Henry Kasa <henrykasa@berkeley.edu>
2020-11-19 19:26:39 -0800

    henry fill_matrix

--- matrix.c
+++ matrix.c
@@ -99,6 +99,7 @@ int allocate_matrix(matrix **mat, int rows, int cols) {
 int allocate_matrix_ref(matrix **mat, matrix *from, int row_offset, int col_offset,
                         int rows, int cols) {
     /* TODO: YOUR CODE HERE */
+
 }
 
 /*
@@ -128,6 +129,7 @@ double get(matrix *mat, int row, int col) {
  */
 void set(matrix *mat, int row, int col, double val) {
     /* TODO: YOUR CODE HERE */
+    
 }
 
 /*
@@ -135,6 +137,14 @@ void set(matrix *mat, int row, int col, double val) {
  */
 void fill_matrix(matrix *mat, double val) {
     /* TODO: YOUR CODE HERE */
+    int row  = mat->rows;
+    int col = mat->cols;
+    for (int i = 0; i<row; i++) {
+        for (int j = 0; j<col; j++) {
+            set(mat, i, j, val);
+        }
+    }
+    
 }
 
 /*
