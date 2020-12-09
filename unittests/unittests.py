cd20c05c3c2be6334a2baa6ffcbca57878c00d99
Ashvin Dhawan <ashvindhawan@berkeley.edu>
2020-12-08 18:01:39 -0800

    local changes, henry pushed

--- unittests/unittests.py
+++ unittests/unittests.py
@@ -74,16 +74,16 @@ class TestNeg(TestCase):
 class TestMul(TestCase):
     def test_small_mul(self):
         # TODO: YOUR CODE HERE
-        dp_mat1, nc_mat1 = rand_dp_nc_matrix(2, 2, seed=0)
-        dp_mat2, nc_mat2 = rand_dp_nc_matrix(2, 2, seed=1)
+        dp_mat1, nc_mat1 = rand_dp_nc_matrix(4, 4, seed=0)
+        dp_mat2, nc_mat2 = rand_dp_nc_matrix(4, 4, seed=1)
         is_correct, speed_up = compute([dp_mat1, dp_mat2], [nc_mat1, nc_mat2], "mul")
         self.assertTrue(is_correct)
         print_speedup(speed_up)
 
     def test_medium_mul(self):
         # TODO: YOUR CODE HERE
-        dp_mat1, nc_mat1 = rand_dp_nc_matrix(1000, 1000, seed=1)
-        dp_mat2, nc_mat2 = rand_dp_nc_matrix(1000, 1000, seed=2)
+        dp_mat1, nc_mat1 = rand_dp_nc_matrix(1024, 1024, seed=1)
+        dp_mat2, nc_mat2 = rand_dp_nc_matrix(1024, 1024, seed=2)
         is_correct, speed_up = compute([dp_mat1, dp_mat2], [nc_mat1, nc_mat2], "mul")
         self.assertTrue(is_correct)
         print_speedup(speed_up)
